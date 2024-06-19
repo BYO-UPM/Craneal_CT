5b4eb16d562a5a17c1ab70abe28626927bfba9bb
aguerrerolopez <alexjorguer@tsc.uc3m.es>
2024-06-19 17:21:38 +0200

    dataset clean and updated

--- Codes/Data_Processing/calculate_dice.py
+++ Codes/Data_Processing/calculate_dice.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import re
 
+
 # DICE
 def dice_coefficient(y_true, y_pred):
     intersection = np.sum(y_true * y_pred)
@@ -12,21 +13,23 @@ def dice_coefficient(y_true, y_pred):
     dice = (2.0 * intersection) / (union + 1e-8)
     return dice
 
+
 # Extract last number from a string
 def extract_last_number(filename):
-    matches = re.findall(r'\d+', filename)
+    matches = re.findall(r"\d+", filename)
     return int(matches[-1]) if matches else 0
 
-path_groundtruth = '/home/ysun@gaps_domain.ssr.upm.es/Craneal_CT/CAT_scans_Preprocessed/S090981/Mask'
-path_prediction = '/home/ysun@gaps_domain.ssr.upm.es/Craneal_CT/result_e3_AUFL_DA_cv/cv9'
+
+path_groundtruth = "./Craneal_CT/CAT_scans_Preprocessed/S090981/Mask"
+path_prediction = "./Craneal_CT/result_e3_AUFL_DA_cv/cv9"
 
 # Order the files
 pred_mask = os.listdir(path_prediction)
 pred_mask = sorted(pred_mask, key=extract_last_number)
 gt_mask = os.listdir(path_groundtruth)
 gt_mask = sorted(gt_mask, key=extract_last_number)
-pred_mask = [file for file in pred_mask if file.endswith('.png')]
-gt_mask = [file for file in gt_mask if file.endswith('.png')]
+pred_mask = [file for file in pred_mask if file.endswith(".png")]
+gt_mask = [file for file in gt_mask if file.endswith(".png")]
 
 dice_coefficients = []
 
@@ -43,9 +46,9 @@ for pred_file, gt_file in zip(pred_mask, gt_mask):
     binary_image1 = gray_image1 > threshold1
     binary_image2 = gray_image2 > threshold2
 
-    dice = dice_coefficient(binary_image1,binary_image2)
+    dice = dice_coefficient(binary_image1, binary_image2)
     dice_coefficients.append(dice)
-    #print(dice)
+    # print(dice)
 
 avg_dice_coefficient = np.mean(dice_coefficients)
 std_dice_coefficient = np.std(dice_coefficients)
